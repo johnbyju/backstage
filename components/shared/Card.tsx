b8bb86bddb17ef8ef9082a2fb0f8b50b41890f2b
johnbyju <johnbyju8@gmail.com>
2024-09-10 09:53:28 +0530

    .

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -8,22 +8,27 @@ type cardPrpos = {
     hidePrice?: boolean
 }
 const Card = ({ event, hidePrice, hasOrderLink }: cardPrpos) => {
-    console.log(event.imageUrl);
-    
+    // console.log(event.imageUrl);
+
     return (
-        <div className='group relative '>
+        <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
             <Link
                 href={`/events/${event._id}`}
-                style={{ backgroundImage: `url(${event.imageUrl})` }} />
+                style={{ backgroundImage: `url(${event.imageUrl})` }}
+                className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
+            />
+
             {/* IS EVENT CREATOR */}
 
             <Link
-                href={`/events/${event._id}`} className='flex min-h-[230px]  flex-col gap-3 p-5 md:gap-4'>
+                href={`/events/${event._id}`}>
                 {!hidePrice && <div className='flex gap-2'>
-                    <span className='p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60'>
-                        {event.isFree ? ("FREE") : (`${event.price}`)}
+                    <span className='p-semibold-14  w-min rounded-full bg-green-100 px-4 py-1 text-green-60'>
+                        {event.isFree ? "FREE" : `${event.price}`}
                     </span>
-                    <p className='p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500'>{event.category.name}</p>
+                    <p className='p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1'>
+                        {event.category.name}
+                    </p>
                 </div>}
             </Link>
 
