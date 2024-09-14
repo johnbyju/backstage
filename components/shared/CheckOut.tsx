7bfb21f586456464330f7e2d4dd1806ac7242c6f
johnbyju <johnbyju8@gmail.com>
2024-09-14 23:49:48 +0530

    modified in componet

--- components/shared/CheckOut.tsx
+++ components/shared/CheckOut.tsx
@@ -2,25 +2,37 @@ import { IEvent } from '@/lib/database/models/event.model'
 import React, { useEffect } from 'react'
 import { Button } from '../ui/button'
 import {loadStripe} from '@stripe/stripe-js'
+import { checkoutOrder } from '@/lib/actions/order.actions';
 
 loadStripe(
   process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
 );
 
-useEffect( ()=>{
-  const query = new URLSearchParams(window.location.search);
 
-  if(query.get('success')){
-    console.log('order placed! You will Recive an email confirmation')
-  }
-  if(query.get('canceled')){
-    console.log('Order canceled -- continue to shop around and checkout when youre ready ');
-    
-  }
-})
 
 const CheckOut = ({event,userId}:{event:IEvent,userId:string}) => {
+
+  useEffect(()=>{
+    const query = new URLSearchParams(window.location.search);
+  
+    if(query.get('success')){
+      console.log('order placed! You will Recive an email confirmation')
+    }
+    if(query.get('canceled')){
+      console.log('Order canceled -- continue to shop around and checkout when youre ready ');
+      
+    }
+  },[])
   const onCheckout = async()=>{
+    const order = {
+      eventTitle : event.title,
+      eventId :event._id,
+      price : event.price,
+      isFree :event.isFree,
+      buyerId:userId
+    }
+
+    await checkoutOrder(order);
 
   }
   return (
