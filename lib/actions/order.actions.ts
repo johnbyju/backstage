d095ea9be85d455d7f6561eda1f3d04e11be2c0c
johnbyju <johnbyju8@gmail.com>
2024-09-15 01:43:16 +0530

    the stripe-dev added

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,9 +1,12 @@
 'use server'
 
-import { CheckoutOrderParams } from "@/types"
+import { CheckoutOrderParams, CreateOrderParams } from "@/types"
 import Order from "../database/models/order.model"
 import Stripe from 'stripe';
 import { redirect } from "next/navigation";
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database";
+
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
@@ -39,5 +42,22 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
     }
     catch (error) {
         throw error
+    }
+}
+
+export const createOrder =async(order:CreateOrderParams)=>{
+    try{
+        await connectToDatabase();
+
+        const newOrder =await Order.create({
+            ...order,
+            event:order.eventId,
+            buyer:order.buyerId
+        });
+
+        return JSON.parse(JSON.stringify(newOrder));
+    }
+    catch(error){
+        handleError(error)
     }
 }
