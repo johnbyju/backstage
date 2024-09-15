caf97c4dd13576644b196416ee9e45c1898ef0e6
johnbyju <johnbyju8@gmail.com>
2024-09-15 10:57:54 +0530

    error fixed

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,24 +1,23 @@
-import EventForm from '@/components/shared/EventForm'
-import { getEventById } from '@/lib/actions/event.actions'
-import { auth } from '@clerk/nextjs/server'
+import EventForm from '@/components/shared/EventForm';
+import { getEventById } from '@/lib/actions/event.actions';
+import { auth } from '@clerk/nextjs/server';
 
-
-type updateEventProps = {
+type UpdateEventProps = {
     params: {
-        id: string
-    }
-}
+        id: string;
+    };
+};
 
-const UpdateEvent = async ({ params:{id}}: updateEventProps) => {
-
+export default async function UpdateEvent({ params: { id } }: UpdateEventProps) {
     const { sessionClaims } = auth();
     const userId = sessionClaims?.userId as string;
     const event = await getEventById(id);
+
     return (
         <>
             <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
                 <h3 className='wrapper h3-bold text-center sm:text-left'>
-                    Upadte Event
+                    Update Event
                 </h3>
             </section>
             <div className='wrapper my-8'>
@@ -30,7 +29,6 @@ const UpdateEvent = async ({ params:{id}}: updateEventProps) => {
                 />
             </div>
         </>
-    )
-}
+    );
+};
 
-export default UpdateEvent
