8489a4b9ab0a7d6c09cca7ea52f79f38c6d7ca44
johnbyju <johnbyju8@gmail.com>
2024-09-19 11:54:44 +0530

    category fillter added

--- components/shared/CategoryFillter.tsx
+++ components/shared/CategoryFillter.tsx
@@ -8,57 +8,50 @@ import { getAllCategories } from '@/lib/actions/category.actions';
 import { ICategory } from '@/lib/database/models/category.model';
 
 const CategoryFillter = () => {
-    const [category, setCategory] = useState<ICategory[]>([]);
-  const router = useRouter();
-  const searchParams =useSearchParams();
- 
-  useEffect( ()=>{
- 
-    const getCategories =async()=>{
-        const categoryList = await getAllCategories();
-        categoryList && setCategory(categoryList as ICategory[])
+    const [categories, setCategories] = useState<ICategory[]>([]);
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+
+        const getCategories = async () => {
+            const categoryList = await getAllCategories();
+            categoryList && setCategories(categoryList as ICategory[])
+        }
+        getCategories();
+    }, [])
+
+    const onSelectCategory = (category: string) => {
+        let newUrl = '';
+        if (category && category !== 'All') {
+            newUrl = formUrlQuery({
+                params: searchParams.toString(),
+                key: 'category',
+                value: category
+            })
+        }
+        else {
+            newUrl = removeKeysFromQuery({
+                params: searchParams.toString(),
+                keysToRemove: ['category']
+            })
+        }
+        router.push(newUrl, { scroll: false });
     }
-    getCategories();
-  },[])
 
-//   useEffect( ()=>{
-//     const delayDebounce = setTimeout( ()=>{
-//       let newUrl = '';
-//       if(category){
-//          newUrl =formUrlQuery({
-//           params : searchParams.toString(),
-//           key: 'category',
-//           value :category
-//         })
-//       }
-//       else{
-//          newUrl =removeKeysFromQuery({
-//           params : searchParams.toString(),
-//           keysToRemove :['query']
-//         })
-//       }
-//       router.push(newUrl,{scroll :false});
-//     },300)
-
-//     return ()=>clearTimeout(delayDebounce);
-//   },[category,searchParams,router])
-
-    const onSelectCategory = (category :string)=>{
-
-    }
-  return (
-    <Select onValueChange={(value :string)=>onSelectCategory(value)}>
-       <SelectTrigger className='select-filed '>
-        <SelectValue placeholder='category'/>
-       </SelectTrigger>
-       <SelectContent>
-        <SelectItem value='All' className='select-item p-regular-14'>All</SelectItem>
-        {category.map( (e)=>(
-            <SelectItem value={e.name} key={e._id} className='select-item p-regular-14'>{e.name}</SelectItem>
-        ))}
-       </SelectContent>
-    </Select>
-  )
+    return (
+        <Select onValueChange={(value: string) => onSelectCategory(value)}>
+            <SelectTrigger className='select-filed '>
+                <SelectValue placeholder='category' />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value='All' className='select-item p-regular-14'>All</SelectItem>
+                {categories.map((e) => (
+                    <SelectItem value={e.name} key={e._id} className='select-item p-regular-14'>{e.name}</SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    )
 }
 
 export default CategoryFillter
