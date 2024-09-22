210b7ac1cfc08ebe99367c166a8bba4217f01488
johnbyju <johnbyju8@gmail.com>
2024-09-23 02:35:37 +0530

    few changes

--- components/shared/CounterComponet.tsx
+++ components/shared/CounterComponet.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CountUp from 'react-countup';
 
 const CounterSection = () => {
@@ -20,13 +20,28 @@ const CounterSection = () => {
     const [starter, setstarter] = useState(false);
 
     useEffect(() => {
-      const position = 
-    
-      return () => {
-       
-      }
+
+        const handleScroll = () => {
+            const section = document.querySelector('.counter-section'); 
+            if (section) {
+                const rect = section.getBoundingClientRect();
+                if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+                    setstarter(true); 
+                    // setTimeout(() => {
+                    //     setstarter(true);
+                    // },1000);
+                }
+            }
+            else{
+                setstarter(false)
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => window.removeEventListener('scroll', handleScroll)
     }, [])
-    
+
 
     return (
         <section className="section mx-5 counter-section py-10 px-6 bg-gradient-to-r from-[#064c59] via-[#261888] to-[#0e4c4d] text-white rounded-lg">
@@ -34,19 +49,19 @@ const CounterSection = () => {
             <div className="flex flex-col md:flex-row justify-around items-center gap-6">
                 <ul className="flex flex-col md:flex-row gap-6">
                     <li className="text-2xl font-bold text-center">
-                       {starter && <CountUp end={100000} duration={3} separator="," />}+
+                        {starter && <CountUp end={100000} duration={3} separator="," />}+
                         <span className="block text-lg">events</span>
                     </li>
                     <li className="text-2xl font-bold text-center">
-                    {starter &&   <CountUp end={50000} duration={3} separator="," />}+
+                        {starter && <CountUp end={50000} duration={3} separator="," />}+
                         <span className="block text-lg">event planners</span>
                     </li>
                     <li className="text-2xl font-bold text-center">
-                    {starter &&   <CountUp end={165} duration={3} separator="," />}+
+                        {starter && <CountUp end={165} duration={3} separator="," />}+
                         <span className="block text-lg">countries</span>
                     </li>
                     <li className="text-2xl font-bold text-center">
-                    {starter && <CountUp end={1600000} duration={3} separator="," />}+
+                        {starter && <CountUp end={1600000} duration={3} separator="," />}+
                         <span className="block text-lg">attendees</span>
                     </li>
                 </ul>
